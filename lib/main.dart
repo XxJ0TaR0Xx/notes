5e8a553256899df0307aa68337a6c01a5b3a0f78
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-10-21 22:20:45 +0300

    feat: add initialization for project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:notes/presentation/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting('ru_RU', null);
+
   runApp(const App());
 }
