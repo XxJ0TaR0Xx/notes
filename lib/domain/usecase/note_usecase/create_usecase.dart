082555a55e34dd1f58ba49e08fc8471aa97e58b5
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-10-24 00:14:44 +0300

    feat: add create note usecase

---
+++ lib/domain/usecase/note_usecase/create_usecase.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+
+import 'package:notes/core/failure/failure.dart';
+import 'package:notes/core/usecase/usecase.dart';
+import 'package:notes/domain/repositories/note_repository.dart';
+import 'package:notes/utils/impotant_item_enum.dart';
+
+@Injectable()
+class CreateNoteUseCase extends UseCase<Unit, CreateParamsNote> {
+  final NoteRepository noteRepository;
+  CreateNoteUseCase({required this.noteRepository});
+  @override
+  Future<Either<Failure, Unit>> call(CreateParamsNote params) {
+    return noteRepository.createNote(noteParams: params);
+  }
+}
+
+class CreateParamsNote {
+  final bool check;
+  final String data;
+  final DateTime date;
+  final Imp important;
+
+  CreateParamsNote({
+    required this.check,
+    required this.data,
+    required this.date,
+    required this.important,
+  });
+}
