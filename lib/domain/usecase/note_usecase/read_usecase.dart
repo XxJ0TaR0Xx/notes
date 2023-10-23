fe97db67b70b2c8ff56089e9e1c9c21bed822fbe
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-10-24 00:17:48 +0300

    feat: add update note use case

---
+++ lib/domain/usecase/note_usecase/read_usecase.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+import 'package:notes/core/failure/failure.dart';
+import 'package:notes/core/usecase/usecase.dart';
+import 'package:notes/domain/entities/note.dart';
+import 'package:notes/domain/repositories/note_repository.dart';
+
+@Injectable()
+class ReadNoteUseCase extends UseCase<Note, String> {
+  final NoteRepository noteRepository;
+  ReadNoteUseCase({required this.noteRepository});
+
+  @override
+  Future<Either<Failure, Note>> call(String params) {
+    return noteRepository.readNote(noteId: params);
+  }
+}
