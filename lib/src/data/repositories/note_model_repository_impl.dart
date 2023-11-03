978f61cf7eac5a73dd759457bd93df214ab09de4
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-11-03 18:01:39 +0300

    feat: implemeted methods for note model repository

--- lib/src/data/repositories/note_model_repository_impl.dart
+++ lib/src/data/repositories/note_model_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
@@ -40,27 +42,89 @@ class NoteModelRepositoryImpl implements NoteRepository {
   }
 
   @override
-  Future<Either<Failure, Unit>> deleteNote(DeleteNoteUseCaseParams params) {
-    // TODO: implement deleteNote
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> deleteNote(DeleteNoteUseCaseParams params) async {
+    try {
+      final DocumentReference<Map<String, dynamic>> ref = firebaseModule.firebaseFirestore.collection('users').doc(params.userId).collection('notes').doc(params.noteId);
+
+      return await ref.delete().then<Either<Failure, Unit>>((_) {
+        return const Right(unit);
+      }).onError((error, stackTrace) {
+        return const Left(FirebaseInternalFailure());
+      });
+    } catch (_) {
+      return const Left(ExternalFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, Note>> readNote(ReadNoteUseCaseParams params) {
-    // TODO: implement readNote
-    throw UnimplementedError();
+  Future<Either<Failure, Note>> readNote(ReadNoteUseCaseParams params) async {
+    try {
+      final DocumentReference<Map<String, dynamic>> ref = firebaseModule.firebaseFirestore.collection('users').doc(params.userId).collection('notes').doc(params.noteId);
+
+      return await ref.get().then<Either<Failure, Note>>((note) {
+        return Right(NoteModel.fromDoc(note));
+      }).onError((error, stackTrace) {
+        return const Left(FirebaseInternalFailure());
+      });
+    } catch (_) {
+      return const Left(ExternalFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, Unit>> updateNote(UpdateNoteUseCaseParams params) {
-    // TODO: implement updateNote
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> updateNote(UpdateNoteUseCaseParams params) async {
+    try {
+      final DocumentReference<Map<String, dynamic>> ref = firebaseModule.firebaseFirestore.collection('users').doc(params.userId).collection('notes').doc(params.noteId);
+      Note? note;
+
+      ref.get().then((value) {
+        note = NoteModel.fromDoc(value);
+      });
+
+      if (note != null) {
+        Note updateNote = Note(
+          data: params.data ?? note!.data,
+          isComplete: note!.isComplete,
+          priorityType: note!.priorityType,
+        );
+
+        await ref.set(NoteModel.toFirebase(updateNote));
+        return const Right(unit);
+      } else {
+        return const Left(FirebaseInternalFailure());
+      }
+    } catch (_) {
+      return const Left(ExternalFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<Note>>> readAllNote(ReadAllNoteUseCaseParams params) {
-    // TODO: implement readAllNote
-    throw UnimplementedError();
+  Future<Either<Failure, List<Note>>> readAllNote(ReadAllNoteUseCaseParams params) async {
+    try {
+      final CollectionReference<Map<String, dynamic>> ref = firebaseModule.firebaseFirestore.collection('users').doc(params.userId).collection('notes');
+
+      List<Note> listNote = [];
+
+      return await ref.get().then<Either<Failure, List<Note>>>((noteQuerySnapshot) {
+        if (noteQuerySnapshot.docs.isEmpty) {
+          log('is empty');
+          return Right(listNote);
+        } else {
+          log('is NOT empty');
+          noteQuerySnapshot.docs.forEach((doc) {
+            log('Doc is ${doc}');
+            listNote.add(NoteModel.fromDoc(doc));
+            log('Doc is $doc');
+          });
+          log('Do not exsit');
+          return Right(listNote);
+        }
+      }).onError((error, stackTrace) {
+        return const Left(FirebaseInternalFailure());
+      });
+    } catch (_) {
+      return const Left(ExternalFailure());
+    }
   }
 }
 
