af4ecffce6d5b3100502dfa096d37e302b2fc57f
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-11-03 18:16:27 +0300

    feat: add implemented methods for user repository

--- lib/src/data/repositories/user_model_repository_impl.dart
+++ lib/src/data/repositories/user_model_repository_impl.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:notes/core/failure/failure.dart';
 import 'package:notes/core/firebase/firebase_module.dart';
+import 'package:notes/src/data/models/user_model.dart';
 import 'package:notes/src/domain/entities/entities.dart';
 import 'package:notes/src/domain/entities/params_usecases/usecases.dart';
+import 'package:notes/src/domain/failures/user_failures.dart';
 import 'package:notes/src/domain/repositories/user_repository.dart';
 
 @Singleton(as: UserRepository)
@@ -15,21 +18,63 @@ class UserModelRepositoryImpl implements UserRepository {
   });
 
   @override
-  Future<Either<Failure, Unit>> createUser(CreateUserUseCaseParams params) {
-    // TODO: implement createUser
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> createUser(CreateUserUseCaseParams params) async {
+    try {
+      final CollectionReference ref = firebaseModule.firebaseFirestore.collection('users');
+      User user = User(
+        name: params.userName,
+        avatarUrl: params.avatarUrl ?? '',
+      );
+
+      return ref.add(UserModel.toFirebase(user)).then<Either<Failure, Unit>>((_) {
+        return const Right(unit);
+      }).onError((error, stackTrace) {
+        return const Left(FirebaseUserInternalFailure());
+      });
+    } catch (_) {
+      return const Left(ExternalUserFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, User>> getUserById(GetByIdUseCaseParams params) {
-    // TODO: implement getUserById
-    throw UnimplementedError();
+  Future<Either<Failure, User>> getUserById(GetByIdUseCaseParams params) async {
+    try {
+      final DocumentReference<Map<String, dynamic>> ref = firebaseModule.firebaseFirestore.collection('users').doc(params.userId);
+
+      return await ref.get().then<Either<Failure, User>>((user) {
+        return Right(UserModel.fromDocument(user));
+      }).onError((error, stackTrace) {
+        return const Left(FirebaseUserInternalFailure());
+      });
+    } catch (_) {
+      return const Left(ExternalUserFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, Unit>> updateuser(UpdateUserUseCaseParams params) {
-    // TODO: implement updateuser
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> updateuser(UpdateUserUseCaseParams params) async {
+    try {
+      final DocumentReference<Map<String, dynamic>> ref = firebaseModule.firebaseFirestore.collection('users').doc(params.userId);
+      User? user;
+
+      ref.get().then((userFireBase) {
+        user = UserModel.fromDocument(userFireBase);
+      });
+
+      if (user != null) {
+        User updateUser = User(
+          name: params.userName ?? user!.name,
+          avatarUrl: params.avaterUrl ?? user!.avatarUrl,
+        );
+
+        await ref.set(UserModel.toFirebase(updateUser));
+        return const Right(unit);
+      } else {
+        return const Left(FirebaseUserInternalFailure());
+      }
+    } catch (e) {
+      return const Left(ExternalUserFailure());
+    }
   }
 }
 
