5086bbc723b89019818f695f9f7e1792f7a2aaec
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-11-14 19:16:24 +0300

    feat: add methods NotePageController for NotePage

--- lib/src/presentation/page/note_page.dart
+++ lib/src/presentation/page/note_page.dart
@@ -1,64 +1,26 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:intl/intl.dart';
 import 'package:notes/src/domain/entities/enums/priority_type.dart';
+import 'package:notes/src/domain/entities/params_usecases/usecases.dart';
 import 'package:notes/src/presentation/const/app_colors.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:notes/src/presentation/controller/note_page_controller.dart';
 
-class NotePage extends StatefulWidget {
+class NotePage extends StatelessWidget {
   static const String route = '/note';
+  final NotePageController notePageController;
 
-  const NotePage({super.key});
+  NotePage({
+    super.key,
+    required this.notePageController,
+  });
+  final TextEditingController textDataController = TextEditingController();
+  final String userId = 'fglGLyHQ2KqF4lZof2sJ';
 
-  @override
-  State<NotePage> createState() => _NotePageState();
-}
-
-class _NotePageState extends State<NotePage> {
-  PriorityType? priorityType;
-  String text = 'Нет';
-  String dateStr = '';
-  bool switchTurn = false;
-
-  String formatDate(DateTime selectedDate) {
-    final formatter = DateFormat('dd MMMM yyyy', 'ru_RU');
-    return formatter.format(selectedDate);
-  }
-
-  void opencalendar(BuildContext context) async {
-    //TODO: Костыль
-    DateTime? selectedDate = await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return Dialog(
-          child: SizedBox(
-            height: 400,
-            width: double.infinity,
-            child: TableCalendar(
-              firstDay: DateTime.utc(2010, 10, 20),
-              lastDay: DateTime.utc(2040, 10, 20),
-              focusedDay: DateTime.now(),
-              headerVisible: true,
-              daysOfWeekVisible: true,
-              sixWeekMonthsEnforced: true,
-              shouldFillViewport: false,
-              headerStyle: const HeaderStyle(titleTextStyle: TextStyle(fontSize: 20, color: Colors.deepPurple, fontWeight: FontWeight.w800)),
-              calendarStyle: const CalendarStyle(todayTextStyle: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
-              onDaySelected: (selectedDay, focusedDay) {
-                Navigator.of(context).pop(selectedDay);
-              },
-            ),
-          ),
-        );
-      },
-    );
-
-    if (selectedDate != null) {
-      setState(() {
-        dateStr = formatDate(selectedDate); // Форматируйте выбранную дату по вашему желанию
-      });
-    }
-  }
+  final String not = 'Нет';
+  final String low = 'Низкая';
+  final String hight = 'Высокая';
 
   @override
   Widget build(BuildContext context) {
@@ -69,25 +31,40 @@ class _NotePageState extends State<NotePage> {
         elevation: 0.0,
         leading: IconButton(
           onPressed: () {
-            Navigator.of(context).pop(); // Возвращаемся на предыдущий экран
+            /// Возвращаемся на предыдущий экран
+            Navigator.of(context).pop();
           },
           icon: SvgPicture.asset('assets/icons/close.svg'),
         ),
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 16.0),
-            child: TextButton(
-              onPressed: () {
-                // Обработка нажатия кнопки "СОХРАНИТЬ"
+            child: AnimatedBuilder(
+              animation: notePageController,
+              builder: (BuildContext context, Widget? child) {
+                return TextButton(
+                  onPressed: () {
+                    notePageController.createNote(
+                      context: context,
+                      createNoteUseCaseParams: CreateNoteUseCaseParams(
+                        userId: userId,
+                        data: textDataController.text,
+                        dateBeforComplete: notePageController.dateBeforComplete,
+                        isComplete: false,
+                        priorityType: notePageController.priorityType,
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'СОХРАНИТЬ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14.0,
+                      letterSpacing: 0.16,
+                    ),
+                  ),
+                );
               },
-              child: const Text(
-                'СОХРАНИТЬ',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 14.0,
-                  letterSpacing: 0.16,
-                ),
-              ),
             ),
           )
         ],
@@ -108,7 +85,10 @@ class _NotePageState extends State<NotePage> {
                       color: AppColors.backElevatedColor,
                       borderRadius: BorderRadius.circular(8.0),
                     ),
+
+                    /// Вводт описания для заметки
                     child: TextField(
+                      controller: textDataController,
                       autocorrect: true,
                       keyboardType: TextInputType.multiline,
                       maxLines: null,
@@ -131,49 +111,52 @@ class _NotePageState extends State<NotePage> {
               alignment: Alignment.topLeft,
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Важность',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    const SizedBox(height: 4.0),
-                    PopupMenuButton<PriorityType>(
-                      initialValue: priorityType,
-                      onSelected: (PriorityType item) {
-                        setState(
-                          () {
-                            priorityType = item;
+                child: AnimatedBuilder(
+                  animation: notePageController,
+                  builder: (context, child) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Важность',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        const SizedBox(height: 4.0),
+
+                        /// Выплывающее меню для выбора важности заметки
+                        PopupMenuButton<PriorityType>(
+                          initialValue: notePageController.priorityType,
+                          onSelected: (PriorityType priorityType) {
+                            notePageController.popUpMenuChangePriority(priorityType: priorityType);
                           },
-                        );
-                      },
-                      itemBuilder: (context) => <PopupMenuEntry<PriorityType>>[
-                        PopupMenuItem<PriorityType>(
-                          onTap: () => text = 'Нет',
-                          value: PriorityType.not,
-                          child: const Text('Нет'),
-                        ),
-                        PopupMenuItem<PriorityType>(
-                          onTap: () => text = 'Низкая',
-                          value: PriorityType.low,
-                          child: const Text('Низкая'),
-                        ),
-                        PopupMenuItem<PriorityType>(
-                          onTap: () => text = 'Высокая',
-                          value: PriorityType.hight,
-                          child: const Text(
-                            'Высокая',
-                            style: TextStyle(color: Colors.red),
+                          itemBuilder: (context) => <PopupMenuEntry<PriorityType>>[
+                            PopupMenuItem<PriorityType>(
+                              onTap: () => notePageController.popUpMenuChangeText(text: not),
+                              value: PriorityType.not,
+                              child: Text(not),
+                            ),
+                            PopupMenuItem<PriorityType>(
+                              onTap: () => notePageController.popUpMenuChangeText(text: low),
+                              value: PriorityType.low,
+                              child: Text(low),
+                            ),
+                            PopupMenuItem<PriorityType>(
+                              onTap: () => notePageController.popUpMenuChangeText(text: hight),
+                              value: PriorityType.hight,
+                              child: Text(
+                                hight,
+                                style: const TextStyle(color: Colors.red),
+                              ),
+                            ),
+                          ],
+                          child: Text(
+                            notePageController.priorityTypeText,
+                            style: notePageController.stylePioryty(),
                           ),
-                        ),
+                        )
                       ],
-                      child: Text(
-                        text,
-                        style: Theme.of(context).textTheme.bodySmall,
-                      ),
-                    )
-                  ],
+                    );
+                  },
                 ),
               ),
             ),
@@ -192,31 +175,41 @@ class _NotePageState extends State<NotePage> {
                         'Сделать до',
                         style: Theme.of(context).textTheme.bodyMedium,
                       ),
-                      TextButton(
-                        style: ButtonStyle(
-                          padding: MaterialStateProperty.all(EdgeInsets.zero),
-                        ),
-                        onPressed: () {
-                          opencalendar(context);
+
+                      /// Выбор даты для заметки
+                      AnimatedBuilder(
+                        animation: notePageController,
+                        builder: (context, child) {
+                          return Opacity(
+                            opacity: notePageController.switchTurn ? 1.0 : 0.0,
+                            child: TextButton(
+                              style: ButtonStyle(
+                                padding: MaterialStateProperty.all(EdgeInsets.zero),
+                              ),
+                              onPressed: () {
+                                if (notePageController.switchTurn) {
+                                  notePageController.opencalendar(context);
+                                }
+                              },
+                              child: Text(
+                                notePageController.dateBeforCompleteStr,
+                              ),
+                            ),
+                          );
                         },
-                        child: Text(
-                          dateStr,
-                        ),
                       ),
                     ],
                   ),
                   const Spacer(),
-                  Switch(
-                    value: switchTurn,
-                    onChanged: (p) {
-                      setState(() {
-                        switchTurn = !switchTurn;
-                        if (!switchTurn) {
-                          dateStr = '';
-                        } else {
-                          dateStr = 'Выберите дату';
-                        }
-                      });
+
+                  /// Тумблер, что бы можно было выбирать дату
+                  AnimatedBuilder(
+                    animation: notePageController,
+                    builder: (context, child) {
+                      return Switch(
+                        value: notePageController.switchTurn,
+                        onChanged: (value) => notePageController.turnSwitch(),
+                      );
                     },
                   )
                 ],
