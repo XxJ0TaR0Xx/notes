c85d0f89d8142fb37f1a3ce49840a1590e3d8042
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-11-14 19:14:09 +0300

    chore: delete trash

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,10 @@
 import 'dart:async';
-// import 'dart:developer';
 
-// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:notes/core/firebase/firebase_module.dart';
 import 'package:notes/core/services/services.dart';
 import 'package:notes/src/presentation/app.dart';
-// import 'package:uuid/uuid.dart';
-
-//! WORKS ON WEB COUSE OF ANDROID DEPENDENCIES. FIX IT
-
-// FutureOr<void> firebaseTest(String id) async {
-//   log('DEBUG: ID - $id');
-//   await FirebaseFirestore.instance.collection('debug').add({
-//     'doc': id,
-//   }).then(
-//     (value) {
-//       log('DEBUG: SUCCESS');
-//     },
-//   ).onError((error, stackTrace) {
-//     log('DEBUG: ERR - ${error.toString()}');
-//   });
-// }
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,9 +15,6 @@ Future<void> main() async {
 
   // firebase related
   await services<FirebaseModule>().init();
-
-  // final String v4ID = const Uuid().v4();
-  // firebaseTest(v4ID);
 
   runApp(const App());
 }
