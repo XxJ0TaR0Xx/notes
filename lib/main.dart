e572b96e762100e5f576070c9c4f5936d49a50f2
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-11-15 19:35:38 +0300

    chore: delete unuses import

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:notes/core/firebase/firebase_module.dart';
 import 'package:notes/core/services/services.dart';
-import 'package:notes/src/data/repositories/note_model_repository_impl.dart';
-import 'package:notes/src/domain/entities/enums/priority_type.dart';
-import 'package:notes/src/domain/entities/params_usecases/usecases.dart';
-import 'package:notes/src/domain/usecase/note_usecases.dart';
 import 'package:notes/src/presentation/app.dart';
 
 Future<void> main() async {
--- lib/src/presentation/controller/note_page_controller.dart
+++ lib/src/presentation/controller/note_page_controller.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
