86bd99988c264c59b7ac265aa7b8682b20050a61
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-11-16 16:02:59 +0300

    feat: add new AuthorizationPage

--- lib/src/presentation/app.dart
+++ lib/src/presentation/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:notes/core/services/services.dart';
 import 'package:notes/src/presentation/const/app_colors.dart';
+import 'package:notes/src/presentation/controller/authorization_page_controller.dart';
+import 'package:notes/src/presentation/page/authorization_page.dart';
 import 'package:notes/src/presentation/controller/home_page_controller.dart';
 import 'package:notes/src/presentation/controller/note_page_controller.dart';
 import 'package:notes/src/presentation/page/forbidden_page.dart';
@@ -13,6 +15,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: AuthorizationPage.route,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: TextTheme(
@@ -42,6 +45,11 @@ class App extends StatelessWidget {
       onGenerateRoute: (settings) {
         return MaterialPageRoute(builder: (_) {
           switch (settings.name) {
+            case AuthorizationPage.route:
+              return AuthorizationPage(
+                authorizationPageControlle: services<AuthorizationPageControlle>(),
+              );
+
             case HomePage.route:
               return HomePage(
                 homePageController: services<HomePageController>(),
