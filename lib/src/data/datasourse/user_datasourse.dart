111177ac4f0e702065343dbdc10367c37f43c420
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-11-16 18:27:05 +0300

    chore

--- lib/src/data/datasourse/user_datasourse.dart
+++ lib/src/data/datasourse/user_datasourse.dart
@@ -5,20 +5,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 class UserDatasourse {
   final String _keyUserId = 'userId';
 
-  // Сохранение userId в SharedPreferences
-
+  /// Сохранение userId в SharedPreferences
   Future<void> saveUserId(String userId) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setString(_keyUserId, userId);
   }
 
-  // Получение userId из SharedPreferences
+  /// Получение userId из SharedPreferences
   Future<String?> getUserId() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getString(_keyUserId);
   }
 
-  // Удаление userId из SharedPreferences
+  /// Удаление userId из SharedPreferences
   Future<void> removeUserId() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.remove(_keyUserId);
