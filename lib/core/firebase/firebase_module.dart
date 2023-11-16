3a4c5fb23e3f54d02a97809a6723046f9cecb864
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-11-16 16:02:30 +0300

    feat: add fireBaseAuth

--- lib/core/firebase/firebase_module.dart
+++ lib/core/firebase/firebase_module.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:injectable/injectable.dart';
 import 'package:notes/core/firebase/firebase_options.dart';
@@ -9,6 +10,7 @@ import 'package:notes/core/firebase/firebase_options.dart';
 class FirebaseModule {
   late final FirebaseApp firebaseApp;
   late final FirebaseFirestore firebaseFirestore;
+  late final FirebaseAuth auth;
 
   FutureOr<void> init() async {
     firebaseApp = await Firebase.initializeApp(
@@ -16,5 +18,7 @@ class FirebaseModule {
     );
 
     firebaseFirestore = FirebaseFirestore.instance;
+
+    auth = FirebaseAuth.instanceFor(app: firebaseApp);
   }
 }
