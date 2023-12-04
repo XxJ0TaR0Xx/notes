317cf25822274519cd7008503f768bb328eff19d
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-12-04 17:17:46 +0300

    chore

--- lib/src/data/repositories/user_model_repository_impl.dart
+++ lib/src/data/repositories/user_model_repository_impl.dart
@@ -17,7 +17,7 @@ class UserModelRepositoryImpl implements UserRepository {
     required this.firebaseModule,
   });
 
-  @override // ты не используешь id из CreateUserUseCaseParams params а генерируешь новый
+  @override
   Future<Either<Failure, Unit>> createUser(CreateUserUseCaseParams params) async {
     try {
       final DocumentReference<Map<String, dynamic>> ref = firebaseModule.firebaseFirestore.collection('users').doc(params.userId);
@@ -51,10 +51,7 @@ class UserModelRepositoryImpl implements UserRepository {
     }
   }
 
-  @override // используй оптимальный кринж снизу вместо
-  //     ref.get().then((userFireBase) {
-  //       user = UserModel.fromDocument(userFireBase);
-  //     });
+  @override
   Future<Either<Failure, Unit>> updateuser(UpdateUserUseCaseParams params) async {
     try {
       final DocumentReference<Map<String, dynamic>> ref = firebaseModule.firebaseFirestore.collection('users').doc(params.userId);
@@ -73,59 +70,3 @@ class UserModelRepositoryImpl implements UserRepository {
     }
   }
 }
-
-// @Singleton(as: UserRepository)
-// class UserModelRepositoryImpl implements UserRepository {
-//   final FirebaseFirestore firebaseFirestore;
-
-//   UserModelRepositoryImpl({required this.firebaseFirestore});
-
-//   @override
-//   Future<Either<Failure, Unit>> createUser({required CreateParamsUser createParams}) async {
-//     try {
-//       final CollectionReference userCollection = firebaseFirestore.collection('users');
-//       final UserModel userModel = UserModel.fromCreateUserParams(createParams);
-
-//       return userCollection.add(userModel.toFirebase()).then<Either<Failure, Unit>>((_) {
-//         return const Right(unit);
-//       }).onError((error, stackTrace) {
-//         return const Left(FirebaseUserInternalFailure());
-//       });
-//     } catch (_) {
-//       return const Left(ExternalUserFailure());
-//     }
-//   }
-
-//   @override
-//   Future<Either<Failure, User>> getUserById({required String userId}) async {
-//     try {
-//       final DocumentReference userDocument = firebaseFirestore.collection('users').doc(userId);
-
-//       return userDocument.get().then<Either<Failure, User>>((value) {
-//         return Right(UserModel.fromDocument(value));
-//       }).onError((error, stackTrace) {
-//         return const Left(FirebaseUserInternalFailure());
-//       });
-//     } catch (_) {
-//       return const Left(ExternalUserFailure());
-//     }
-//   }
-
-//   @override
-//   Future<Either<Failure, Unit>> updateuser({required UpdateParamsUser updateParams}) async {
-//     try {
-//       Map<String, dynamic> updateData = {};
-//       if (updateParams.avatarUrl != null) updateData['avatarUrl'] = updateParams.avatarUrl;
-//       if (updateParams.name != null) updateData['name'] = updateParams.name;
-
-//       final DocumentReference userDocument = firebaseFirestore.collection('users').doc(updateParams.userId);
-//       return userDocument.update(updateData).then<Either<Failure, Unit>>((_) {
-//         return const Right(unit);
-//       }).onError((error, stackTrace) {
-//         return const Left(FirebaseUserInternalFailure());
-//       });
-//     } catch (_) {
-//       return const Left(ExternalUserFailure());
-//     }
-//   }
-// }
