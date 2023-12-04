75d67db4df1114f8fc0a02926dc2706f341167cc
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-12-04 17:19:02 +0300

    feat: add firebaseSingInFailure

---
+++ lib/src/domain/failures/auth_failures.dart
@@ -0,0 +1,9 @@
+import 'package:notes/core/failure/failure.dart';
+
+final class FirebaseSingInFailure extends Failure {
+  const FirebaseSingInFailure();
+}
+
+final class FirebaseSingInAnonymouslyFailure extends Failure {
+  const FirebaseSingInAnonymouslyFailure();
+}
